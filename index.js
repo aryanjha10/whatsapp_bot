04433ac54fa07b1569706560a0532210348705a6
aryanjha10 <aryan@organicinbound.io>
2025-05-09 14:11:54 +0530

    Added don't run sandbox in index.js

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-
 const client = new Client({
-    authStrategy: new LocalAuth(),
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  }
 });
 
 client.on("qr", (qr) => {
